39752290ca2688b838bff821dd44e531d4d6543b
iskandar <iskandarovi542@gmail.com>
2024-09-12 10:11:50 +0500

    commit

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tudo_app/injections.dart';
 import 'package:tudo_app/presentation/blocs/events_bloc/todo_events_bloc.dart';
-import 'package:tudo_app/presentation/screens/home_screen/home_screen.dart';
 import 'package:tudo_app/presentation/screens/splash_screen/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
